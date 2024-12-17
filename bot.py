4d1f146a07f7890a36400b454389528fad374c7e
Oᴠᴇʀ ❖ Sᴛʀɪᴘᴇ <120897603+OverStripe@users.noreply.github.com>
2024-12-17 11:03:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ email_username = "songindian16@gmail.com"
 email_password = os.getenv("EMAIL_PASSWORD", "gxzk hegw vbks pavr")  # App Password
 
 # Telegram Bot Token
-telegram_bot_token = "7367928267:AAHh9Qvw-zRm6dIA86ADdcyd2Y12TXkcHfY"  # Replace with your Telegram bot token
+telegram_bot_token = "7367928267:AAGaElGQaoMg2jA9loHuxd7gSoXJ6V2ak7w"  # Replace with your Telegram bot token
 
 # Email settings
-recipient_email = "recover@telegram.org"
+recipient_email = "support@telegram.org"
 subject = "Account Recovery Request"
 
 # Default email message template
